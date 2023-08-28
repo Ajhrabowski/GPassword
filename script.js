27f6edf4cc03b4cf18d1f3dbd46815af843bafc4
Anthony Hrabowski <ajhrabowski@gmail.com>
2023-08-28 18:26:19 -0400

    Moved all codes up. Combined functions that were seperate and merged them. Created a for loop as well.

--- script.js
+++ script.js
@@ -1,39 +1,89 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var availiableCharacters="";
+var passwordLength = 128;
+const lowercase = "abcdefghijklmnopqrstuvwxyz"
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers = "0123456789"
+const symbols = "!@#$%^&*()_+=-<>,./{}[]"
 
 function generatePassword(){
+  let password ="";
   
-  var passwordLength = prompt("How long would you like your pasword to be?");
-  var lowercase = confirm("Do you want lowercase letters?");
-  var uppercase = confirm("Do you want uppercase letters?");
-  var numbers = confirm("Do you want numbers in your password?");
-  var specialcharacters = confirm("Do you want special characters in your password?");
+  passwordLength = prompt("How long would you like your pasword to be?");
+  var lowercaseFlag = confirm("Do you want lowercase letters?");
+  var uppercaseFlag = confirm("Do you want uppercase letters?");
+  var numbersFlag = confirm("Do you want numbers in your password?");
+  var symbolsFlag = confirm("Do you want symbols in your password?");
 
-  if (lowercase){
+ 
+
+
+  if (lowercaseFlag){
+    availiableCharacters += lowercase
     alert("You chose lowercase")
+    password += lowercase[Math.floor(Math.random()*lowercase.length)];
   } 
 
-  if (uppercase){
+  if (uppercaseFlag){
+    availiableCharacters += uppercase
     alert("You chose uppercase")
+    password += uppercase[Math.floor(Math.random()*uppercase.length)];
   } 
   
-  if (numbers){
+  if (numbersFlag){
+    availiableCharacters += numbers
     alert("You chose numbers")
+    password += numbers[Math.floor(Math.random()*numbers.length)];
   } 
 
-  if (specialcharacters){
-    alert("You chose special characters")
+  if (symbolsFlag){
+    availiableCharacters += symbols
+    alert("You chose symbols")
+    password += symbols[Math.floor(Math.random()*symbols.length)];
   } 
+
+  for (var i=password.length; i < passwordLength; i++) {
+   
+    password += availiableCharacters[Math.floor(Math.random()*availiableCharacters.length)];
+
+ }
+
+  return password;
 }
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-}
+
+
+
+
+
+
+var password = generatePassword();
+var passwordText = document.querySelector("#password");
+
+passwordText.value = password;
+
+
+
+
+
+ 
+  
+
+
+
+
+  
+
+
+
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+generateBtn.addEventListener("click", generatePassword);
+
+
+
